0217268b47dab1aa4d5cce6977e88cb167152232
harrishendrato <harris80hnro@gmail.com>
2019-09-12 00:07:34 +0700

    initial commit

--- application/views/user/sale.php
+++ application/views/user/sale.php
@@ -7,7 +7,7 @@
       <!-- Sidebar - Brand -->
       <div class="sidebar-brand d-flex align-items-center justify-content-center">
         <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-cash-register"></i>
+          <i clgit aass="fas fa-cash-register"></i>
         </div>
         <div class="sidebar-brand-text mx-3"><h1>POS</h1></div>
       </div>
