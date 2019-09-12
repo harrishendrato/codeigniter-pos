18ecbda925ab47908ca10e43250fc8ac3084d197
harrishendrato <harris80hnro@gmail.com>
2019-09-12 14:15:46 +0700

    initial commit

--- application/views/user/sale_item.php
+++ application/views/user/sale_item.php
@@ -1,4 +1,4 @@
-  <!-- Page Wrapper -->
+<i class="fas fa-user-circle"></i>  <!-- Page Wrapper -->
   <div id="wrapper">
 
     <!-- Sidebar -->
@@ -78,7 +78,7 @@
                       <li class="nav-item dropdown no-arrow">
                         <a href="#" class="nav-link dropdown-toggle" id="user" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                           <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo "<b>" .$this->session->userdata('name'); ?></b></span>
-                          <img class="img-profile rounded-circle">
+                          <i class="fas fa-user-circle"></i>
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
